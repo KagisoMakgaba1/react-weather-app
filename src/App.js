df8bc956db44704fae63aebf2f62ef521008888c
kagisomakgaba <kagisomakgaba99@gmail.com>
2023-12-24 21:21:32 +0200

    Added html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Weather App</h1>
       </header>
+      <p>This was coded by Kagiso Makgaba, open-sourced on <a href="/"> Github </a>and hosted on <a href="/">Netlify</a></p>
     </div>
   );
 }
