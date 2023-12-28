a3a42d34517dd90fa28cbf0311902ef77438f12e
kagisomakgaba <kagisomakgaba99@gmail.com>
2023-12-28 22:28:03 +0200

    API Intergration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity="Pretoria" />
       </div>
       <p>
         This was coded by Kagiso Makgaba, open-sourced on{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherIcon from "./WeatherIcon";
+import FormatDate from "./FormatDate";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="weather">
-      <div className="row">
-        <div className="col-8">
-          <form className="mt-3">
-            <div className="row">
-              <div className="col-10">
-                <input
-                  type="search"
-                  placeholder="Enter city name.."
-                  className="form-control"
-                  id="search"
-                  autoFocus="on"
-                />
+export default function Weather(props) {
+  const [data, setData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    console.log(response.data.dt * 1000);
+    setData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      temp_max: response.data.main.temp_max,
+      temp_min: response.data.main.temp_min,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    let apiKey = "f606a147394d27601839ffd5343624ee";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (data.ready) {
+    return (
+      <div className="weather">
+        <div className="row">
+          <div className="col-8">
+            <form className="mt-3" onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-10">
+                  <input
+                    type="search"
+                    placeholder="Enter city name.."
+                    className="form-control"
+                    id="search"
+                    autoFocus="on"
+                    onChange={handleCityChange}
+                  />
+                </div>
+                <div className="col-2">
+                  <button className="icon" type="submit">
+                    <i className="fa-solid fa-search"></i>
+                  </button>
+                </div>
               </div>
-              <div className="col-2">
-                <button className="icon" type="submit">
-                  <i className="fa-solid fa-search"></i>
-                </button>
-              </div>
+            </form>
+            <br />
+            <h1>{data.city}</h1>
+            <ul>
+              <li>
+                <FormatDate date={data.date} />
+              </li>
+            </ul>
+
+            <div className="temp">
+              <span className="icon">
+                <WeatherIcon code={data.icon} size={82} />
+              </span>
+              <span className="main-temp">{Math.round(data.temperature)}</span>{" "}
+              <sup>℃</sup> <sup>|</sup>
+              <sup> ℉</sup>
             </div>
-          </form>
-          <br />
-          <h1>Johannesburg</h1>
-          <ul>
-            <li>Tuesday 20:00</li>
-          </ul>
-          <div className="temp">
-            <span>
-              <i class="fa-regular fa-sun fa-2x"></i>
-            </span> 
-            <span className="main-temp">18</span> <sup>℃</sup> <sup>|</sup>
-            <sup> ℉</sup>
+            <div className="description">{data.description}</div>
           </div>
-          <div className="description">Showers and Thundershowers</div>
-        </div>
-        <div className="col-4 mt-5 info">
-          <div className="more-info">Today's Weather Condition</div>
-          <hr />
-          <ul>
-            <li>Humidity: 85%</li>
+          <div className="col-4 mt-5 info">
+            <div className="more-info">Today's Weather Condition</div>
             <hr />
-            <li>Min Temperature: 17℃</li>
-            <hr />
-            <li>Max Temperature: 27℃</li>
-            <hr />
-            <li>Wind Speed: 9 km/h</li>
-          </ul>
+            <ul>
+              <li>Humidity: {data.humidity}%</li>
+              <hr />
+              <li>Min Temperature: {Math.round(data.temp_min)}℃</li>
+              <hr />
+              <li>Max Temperature: {Math.round(data.temp_max)}℃</li>
+              <hr />
+              <li>Wind Speed: {Math.round(data.wind)} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading..";
+  }
 }
